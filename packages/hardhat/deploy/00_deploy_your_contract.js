572232297e5939d0931ee7739edea5f3be6cab9e
shafayeat sumit <shafayeatkabir@gmail.com>
2022-06-05 14:38:23 +0600

    new deployment script for factory contract

--- packages/hardhat/deploy/00_deploy_your_contract.js
+++ packages/hardhat/deploy/00_deploy_your_contract.js
@@ -1,16 +1,14 @@
 // deploy/00_deploy_your_contract.js
 
-module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
+module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
-  const chainId = await getChainId();
 
-  await deploy("MultiSigWallet", {
+  await deploy("MultiSigFactory", {
     from: deployer,
-    // args: [chainId, ["your contract address"], number_of_signers],
-    args: [chainId, ["0x13E19A5B1AFAAF6Ca59E3FeaBaC71c46F123471f"], 1],
     log: true,
     waitConfirmations: 5,
   });
 };
-module.exports.tags = ["MultiSigWallet"];
+
+module.exports.tags = ["MultiSigFactory"];
